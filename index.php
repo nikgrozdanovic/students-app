3696fe64955a022f4159621605858528ba402f1d
Nikola Grozdanovic <nutzington@gmail.com>
2020-11-21 16:41:27 +0100

    Changing the route

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once realpath('vendor/autoload.php');
 $router = new \Bramus\Router\Router();
 
 // Define routes
-$router->get('/students/(\d+)', function($id) {
+$router->get('/test/students/(\d+)', function($id) {
     $students = new Student;
 
     echo $students->show($id);
